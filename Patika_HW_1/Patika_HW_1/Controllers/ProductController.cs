c2ba7ffef113f54de43607038c9801761679c816
Mustafa Akin Akarsu <akarsumustafa0153@gmail.com>
2024-07-03 20:15:33 +0300

    Initial commit

---
+++ Patika_HW_1/Patika_HW_1/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Patika_HW_1.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+	private static readonly List<Product> Products = new();
+
+	[HttpGet("{id}")]
+	public IActionResult Get(int id)
+	{
+		var product = Products.FirstOrDefault(p => p.Id == id);
+		if (product == null)
+		{
+			return NotFound();
+		}
+		return Ok(product);
+	}
+
+	[HttpPost]
+	public IActionResult Post([FromBody] Product product)
+	{
+		Products.Add(product);
+		return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+	}
+
+	[HttpPut("{id}")]
+	public IActionResult Put(int id, [FromBody] Product updatedProduct)
+	{
+		var product = Products.FirstOrDefault(p => p.Id == id);
+		if (product == null)
+		{
+			return NotFound();
+		}
+		product.Name = updatedProduct.Name;
+		product.Price = updatedProduct.Price;
+		return NoContent();
+	}
+
+	[HttpDelete("{id}")]
+	public IActionResult Delete(int id)
+	{
+		var product = Products.FirstOrDefault(p => p.Id == id);
+		if (product == null)
+		{
+			return NotFound();
+		}
+		Products.Remove(product);
+		return NoContent();
+	}
+
+
+	[HttpGet("list")]
+	public IActionResult List([FromQuery] string name)
+	{
+		var products = Products.Where(p => string.IsNullOrEmpty(name) || p.Name.Contains(name)).ToList();
+		return Ok(products);
+	}
+}
---
+++ Patika_HW_1/Patika_HW_1/Validations/ProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Patika_HW_1.Models;
+
+namespace Patika_HW_1.Validations
+{
+	public class ProductValidator : AbstractValidator<Product>
+	{
+		public ProductValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+			RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+		}
+	}
+}
